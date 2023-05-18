83515dc56daf54fb9fa9ca88e510a4c287f0a5e6
Vikas Sharma <59821960+VikasSharma707@users.noreply.github.com>
2023-05-18 16:17:31 +0000

    New react

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ColorfulText from './ColorfulText';
+
+ReactDOM.render(<ColorfulText />, document.getElementById('root'));
